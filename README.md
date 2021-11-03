6f323faa5fe4b455ed94ebbd7e2bcbe0dd69da24
Arnas Smicius <henrikas.smicius@vinted.com>
2021-11-03 16:37:20 +0200

    Build and reuse Todo component

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Continue course at
 
-https://youtu.be/Dorf8i6lCuk?t=2870
+https://youtu.be/Dorf8i6lCuk?t=3309
 
 # Getting Started with Create React App
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import Todo from './components/Todo';
 
 function App() {
-  return (<div>
-    <h1>My Todos</h1>
-    <div className='card'>
-      <h2>TITLE</h2>
-      <div className='actions'>
-        <button className='btn'>Delete</button>
-      </div>
-    </div>
-  </div>);
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+    </div>);
 }
 
 export default App;
